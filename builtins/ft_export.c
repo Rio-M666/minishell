26d232405703ac2d6d21c2aa3145d06beea163c6
Toshiki Yamaguchi <toyamagu@c1r7s1.42tokyo.jp>
2026-01-25 00:36:43 +0900

    pipe_fix_&&_builtin_make

---
+++ builtins/ft_export.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_export.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: toyamagu <toyamagu@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/25 00:35:44 by toyamagu          #+#    #+#             */
+/*   Updated: 2026/01/25 00:36:02 by toyamagu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+static int	is_numeric(char *str)
+{
+	int	i;
+
+	i = 0;
+	if (str[i] == '-' || str[i] == '+')
+		i++;
+	while (str[i])
+	{
+		if (!ft_isdigit(str[i]))
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+int	ft_exit(char **args, t_shell *shell)
+{
+	(void)shell;
+	ft_putstr_fd("exit\n", 1);
+	if (args[1])
+	{
+		if (!is_numeric(args[1]))
+		{
+			ft_putstr_fd("minishell: exit: ", 2);
+			ft_putstr_fd(args[1], 2);
+			ft_putstr_fd(": numeric argument required\n", 2);
+			exit(255);
+		}
+		if (args[2])
+		{
+			ft_putstr_fd("minishell: exit: too many arguments\n", 2);
+			return (1);
+		}
+		exit(ft_atoi(args[1]) % 256);
+	}
+	exit(shell->last_status);
+}
