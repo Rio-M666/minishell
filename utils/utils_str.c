086bc5f5e7c0a6a335a4c88002c2d5068bfd347d
Toshiki Yamaguchi <toyamagu@c6r1s3.42tokyo.jp>
2026-01-29 19:24:51 +0900

    strncmp

--- utils/utils_str.c
+++ utils/utils_str.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   utils_str.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mrio <mrio@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: toyamagu <toyamagu@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/23 18:46:51 by mrio              #+#    #+#             */
-/*   Updated: 2026/01/23 18:46:52 by mrio             ###   ########.fr       */
+/*   Updated: 2026/01/29 19:24:23 by toyamagu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -78,4 +78,22 @@ int	ft_atoi(const char *str)
 		str++;
 	}
 	return (result * sign);
+}
+
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	size_t i;
+	unsigned char *a;
+	unsigned char *b;
+
+	a = (unsigned char *)s1;
+	b = (unsigned char *)s2;
+	i = 0;
+	while (i < n)
+	{
+		if (a[i] != b[i])
+			return (a[i] - b[i]);
+		i++;
+	}
+	return (0);
 }
